f32d648139819683a2abcb04284d37f860b49daa
Radek <radoslaw.czajor@pl.bosch.com>
2024-09-20 11:22:49 +0200

    Added sealed-interfaces comment

--- sealed-interfaces/src/main/java/com/czajor/Result.java
+++ sealed-interfaces/src/main/java/com/czajor/Result.java
@@ -9,3 +9,8 @@ record Success<T>(T data) implements Result<T> {
 
 record Failure<T>(RuntimeException exception) implements Result<T> {
 }
+
+//record NotSuccessNorFailure<T>() implements Result<T> {
+//}  <-- we cannot add new one, as interface doesn't permit that
+
+// test included in test package
